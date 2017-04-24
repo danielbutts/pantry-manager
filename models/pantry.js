8aa59d7982474f5a57bfb0850ecfa2dcd3cb7f24
danielbutts <daniel.butts@gmail.com>
2017-04-24 11:12:08 -0700

    corrected model names and table reference

--- models/pantry.js
+++ models/pantry.js
@@ -1,12 +1,12 @@
 const sequelize = require('../db/connection');
 
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('pantry', {
+  return sequelize.define('pantries', {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     createdAt: { type: 'TIMESTAMP', allowNull: false, },
     updatedAt: { type: 'TIMESTAMP', allowNull: false, },
     user_id: { type: DataTypes.INTEGER, references: {
-      model: 'User',
+      model: 'user',
       key: 'id',
       deferrable: sequelize.Deferrable.INITIALLY_IMMEDIATE,
     }},
