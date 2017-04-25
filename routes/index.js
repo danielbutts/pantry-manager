4784a46904a14747ae4ba4dfd6d9a5b8b56b5aaf
Sean McDermott <mcderm.sean@gmail.com>
2017-04-25 16:05:11 -0700

    added new user registration form

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ router.get('/', (req, res) => {
   res.render('pages/index', { title: 'Pantry Weasel' });
 });
 
+router.get('/new-user', (req, res) => {
+  res.render('pages/new-user', { title: 'Pantry Weasel' });
+});
+
 module.exports = router;
