4378187bb26f30f0e00776c1dc59ec21f323b732
danielbutts <daniel.butts@gmail.com>
2017-04-24 21:54:08 -0700

    Added items routes and refactored templates
    - created get/post/put/delete routes for items
    - moved EJS templates into pages and partials directories within views
    - updated items seeded to capitalize names

--- seeders/20170424205438-items.js
+++ seeders/20170424205438-items.js
@@ -8,14 +8,14 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
     return [
       queryInterface.bulkInsert('items', [
-        { pantry_id: 1, name: 'celery', quantity: 3, percent_used: 0, units: 'cup', expire_date: later},
-        { pantry_id: 1, name: 'chicken', quantity: 1, percent_used: 0, units: 'lb', expire_date: later},
-        { pantry_id: 1, name: 'onion', quantity: 2, percent_used: 0, expire_date: later},
-        { pantry_id: 1, name: 'carrots', quantity: 2, percent_used: 0, units: 'lb', expire_date: later},
-        { pantry_id: 2, name: 'apples', quantity: 1, percent_used: 0, units: 'lb', expire_date: later},
-        { pantry_id: 2, name: 'bread', quantity: 1, percent_used: 0, units: 'loaf', expire_date: later},
-        { pantry_id: 2, name: 'butter', quantity: 3, percent_used: 0, units: 'stick', expire_date: later},
-        { pantry_id: 2, name: 'apples', quantity: 1, percent_used: 0, units: 'lb', expire_date: later},
+        { pantry_id: 1, name: 'Celery', quantity: 3, percent_used: 0, units: 'cup', expire_date: later},
+        { pantry_id: 1, name: 'Chicken', quantity: 1, percent_used: 0, units: 'lb', expire_date: later},
+        { pantry_id: 1, name: 'Onion', quantity: 2, percent_used: 0, expire_date: later},
+        { pantry_id: 1, name: 'Carrots', quantity: 2, percent_used: 0, units: 'lb', expire_date: later},
+        { pantry_id: 2, name: 'Apples', quantity: 1, percent_used: 0, units: 'lb', expire_date: later},
+        { pantry_id: 2, name: 'Bread', quantity: 1, percent_used: 0, units: 'loaf', expire_date: later},
+        { pantry_id: 2, name: 'Butter', quantity: 3, percent_used: 0, units: 'stick', expire_date: later},
+        { pantry_id: 2, name: 'Apples', quantity: 1, percent_used: 0, units: 'lb', expire_date: later},
       ])
     ];
   },
