e029aeb004ce5b8c92abbda6d8f133610a916260
Sean McDermott <mcderm.sean@gmail.com>
2017-04-26 09:23:56 -0700

    added error handling for login page

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('pages/index', { title: 'Pantry Weasel' });
+  res.render('pages/index', { title: 'Pantry Weasel', error: '' });
 });
 
 router.get('/new-user', (req, res) => {
-  res.render('pages/new-user', { title: 'Pantry Weasel' });
+  res.render('pages/new-user', { title: 'Pantry Weasel', error: '' });
 });
 
 module.exports = router;
