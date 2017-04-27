6d34290c9c48b52837146193b2bcc63fb908bdb7
danielbutts <daniel.butts@gmail.com>
2017-04-27 16:45:29 -0700

    refactored search into partial shared by index and recipes page. return 5 recent searches to home page without logged in user.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,55 @@
 const express = require('express');
+const sequelize = require('../db/connection');
+const models = require('../models')(sequelize);
+// const rp = require('request-promise');
 
+if (process.env.NODE_ENV !== 'production' && !process.env.IS_BUILD) {
+  require('dotenv').config(); // eslint-disable-line global-require
+}
 const router = express.Router();
 
 
 /* GET home page. */
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   const currentUser = {
     firstName: req.session.firstName,
     userId: req.session.userId,
   };
-  res.render('pages/index', { title: 'Pantry Weasel', error: '', currentUser });
+  models.Recipe.findAll({
+    limit: 5,
+    order: [['createdAt', 'DESC']],
+  })
+  .then((recipeResults) => {
+    const recipes = {};
+    const ingredientQueries = [];
+
+    recipeResults.forEach((recipeResult) => {
+      const recipe = recipeResult.dataValues;
+      // console.log(recipe);
+      recipes[recipe.id] = recipe;
+      ingredientQueries.push(recipeResult.getIngredients());
+    });
+
+    Promise.all(ingredientQueries)
+    .then((ingredientsFromResults) => {
+      ingredientsFromResults.forEach((ingredientsResults) => {
+        // console.log(ingredientsResults);
+        const ingredients = [];
+        const recipeId = ingredientsResults[0].dataValues.recipeId;
+        ingredientsResults.forEach((ingredientResult) => {
+          ingredients.push(ingredientResult.dataValues.name);
+        });
+        recipes[recipeId].ingredients = ingredients;
+      });
+      // console.log(recipes);
+      const recent = Object.values(recipes);
+      console.log(recent);
+      res.render('pages/index', { title: 'Pantry Weasel', error: '', currentUser, recent });
+    });
+  })
+  .catch((err) => {
+    next(err);
+  });
 });
 
 router.get('/new-user', (req, res) => {
