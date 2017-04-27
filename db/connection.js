435084442f80ce6a51c1f26a9e00b4a7765b0702
danielbutts <daniel.butts@gmail.com>
2017-04-27 11:12:48 -0700

    config changes for sequelize on heroku

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,16 @@
 const Sequelize = require('sequelize');
 
-const DB_TYPE = 'postgres';
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 5432;
+let sequelize;
 
-const DB_NAME = process.env.DB_NAME;
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
-
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: DB_TYPE,
-});
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging: true, //false
+  });
+} else {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+}
 
 module.exports = sequelize;
--- models/Ingredient.js
+++ models/Ingredient.js
@@ -3,7 +3,7 @@ const sequelize = require('../db/connection');
 const Sequelize = sequelize.Sequelize;
 
 const Ingredient = sequelize.define('ingredients', {
-  name: { type: Sequelize.DataTypes.STRING(50), allowNull: false },
+  name: { type: Sequelize.DataTypes.STRING(255), allowNull: false },
   createdAt: { type: 'TIMESTAMP', allowNull: false, defaultValue: sequelize.literal('CURRENT_TIMESTAMP')},
   updatedAt: { type: 'TIMESTAMP', allowNull: false, defaultValue: sequelize.literal('CURRENT_TIMESTAMP')},
 }, {
