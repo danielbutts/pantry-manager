7ec8bdc9aea859a95965d643b706d274d762bd7e
Sean McDermott <mcderm.sean@gmail.com>
2017-04-27 19:00:32 -0700

    enter key now adds item

--- public/scripts/pantry.js
+++ public/scripts/pantry.js
@@ -1,4 +1,13 @@
-$('#add-pantry-item').click((e) => {
+const deleteItem = () => {
+  const $id = $(event.target).data('id');
+  $.ajax({
+    method: 'DELETE',
+    url: `/items/${$id}`,
+  })
+  $(event.target).closest('.pantry-item').remove();
+}
+
+const addItem = (e) => {
   e.preventDefault();
   const $item = $('#item-name').val();
   const $pantryId = $('input.pantry-id').val();
@@ -10,13 +19,15 @@ $('#add-pantry-item').click((e) => {
   .done(() => {
     window.location.replace(`/users/${$pantryId}/pantries/${$pantryId}`);
   })
+}
+
+$('#add-pantry-item').click(addItem);
+
+$('#item-name').keypress((e) => {
+  if (e.which == 13) {
+    $('#add-pantry-item').click();
+  }
 });
 
-$('.pantry-item .remove').on('click', () => {
-  const $id = $(event.target).data('id');
-  $.ajax({
-    method: 'DELETE',
-    url: `/items/${$id}`,
-  })
-  $(event.target).closest('.pantry-item').remove();
-})
+
+$('.pantry-item .remove').on('click', deleteItem)
